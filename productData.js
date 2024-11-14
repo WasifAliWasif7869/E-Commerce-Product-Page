f8ea06d0fa558f2bef99e6ea7a9b489089ea8e36
Wasif Ali <wasifaliwasif7869@gmail.com>
2024-11-14 17:18:04 +0700

    Update productData.js

--- productData.js
+++ productData.js
@@ -2,7 +2,7 @@ export default {
   productID: 1,
   name: "Fall Limited Edition Sneakers",
   description: `These low-profile sneakers are your perfect casual wear companion.
-            Featuring a durable rubber outer sole, they'll withstand everything
+            Featuring a durable rubber outer sole, they will withstand everything
             the weather can offer.`,
   price: 125.0,
   imageURL: [
